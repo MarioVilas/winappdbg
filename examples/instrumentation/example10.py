8aca303a0fccd8eec3252ed7a135cc6ec7ce04c8
Mario Vilas <mvilas@gmail.com>
2009-04-06 13:47:30 +0000

    updated examples

---
+++ examples/instrumentation/example10.py
@@ -0,0 +1,36 @@
+# Example #10
+# http://apps.sourceforge.net/trac/winappdbg/wiki/wiki/Instrumentation#Example10:resolveanAPIfunctioninaprocess
+
+from winappdbg import Process
+
+def print_api_address( pid, modName, procName ):
+    
+    # Request debug privileges
+    System.request_debug_privileges()
+    
+    # Instance a Process object
+    process = Process( pid )
+    
+    # Lookup it's modules
+    process.scan_modules()
+    
+    # Get the module
+    module = process.get_module_by_name( modName )
+    if not module:
+        print "Module not found: %s" % modName
+        return
+    
+    # Resolve the requested API function address
+    address = module.resolve( procName )
+    
+    # Print the address
+    print "%s!%s == 0x%.08x" % ( modName, procName, address )
+
+# When invoked from the command line,
+# the first argument is a process ID
+if __name__ == "__main__":
+    import sys
+    pid      = int( sys.argv[1] )
+    modName  = sys.argv[2]
+    procName = sys.argv[3]
+    print_api_address( pid, modName, procName )
