5c89df747b08b7bd349de56beff752661352ffe3
Mario Vilas <mvilas@gmail.com>
2009-04-16 09:36:27 +0000

    updated examples

---
+++ examples/debugging/example8.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2009, Mario Vilas
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice,this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# $Id$
+
+# Example #8
+# http://apps.sourceforge.net/trac/winappdbg/wiki/wiki/Debugging#Example8:settingabreakpoint
+
+from winappdbg import Debug, EventHandler
+
+
+def action_callback( event ):
+    
+    # Get some event information
+    address = event.get_thread().get_stack_dwords(1)[0]
+    tid     = event.get_tid()
+    pid     = event.get_pid()
+    
+    # Show a message to the user
+    message = "kernel32!CreateFileW called from 0x%.08x by thread %d at process %d"
+    message = message % ( address, tid, pid )
+    print message
+
+
+class MyEventHandler( EventHandler ):
+    
+    def load_dll( self, event ):
+        
+        # Get the new module object
+        module = event.get_module()
+        
+        # If it's kernel32.dll...
+        if module.match_name("kernel32.dll"):
+            
+            # Get the address of CreateFile
+            address = module.resolve( "CreateFileW" )
+            
+            # Set a breakpoint at CreateFile
+            event.debug.break_at( event.get_pid(), address, action_callback )
+            
+            # If you use stalk_at instead of break_at,
+            # the message will only be shown once
+            #
+            # event.debug.stalk_at( event.get_pid(), address, action_callback )
+
+    
+def simple_debugger( argv ):
+    
+    # Instance a Debug object, passing it the MyEventHandler instance
+    debug = Debug( MyEventHandler() )
+    
+    # Start a new process for debugging,
+    # and automatically attach to it's child processes
+    debug.execv( argv, bFollow = True )
+    
+    # Wait for the debugee to finish
+    debug.loop()
+
+
+# When invoked from the command line,
+# the first argument is an executable file,
+# and the remaining arguments are passed to the newly created process
+if __name__ == "__main__":
+    import sys
+    simple_debugger( sys.argv[1:] )
