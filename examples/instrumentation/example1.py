2e8e57a4216ee75ea2f02e95ecac6c63a37378b6
Mario Vilas <mvilas@gmail.com>
2009-12-01 03:18:32 +0000

    fixed get_wait_chain() plus updated examples 1 and 3 from the instrumentation section

--- examples/instrumentation/example1.py
+++ examples/instrumentation/example1.py
@@ -40,9 +40,6 @@ System.request_debug_privileges()
 # Create a system snaphot
 system = System()
 
-# The snapshot is initially empty, so populate it
-system.scan_processes()
-
 # Now we can enumerate the running processes
 for process in system:
     print "%d:\t%s" % ( process.get_pid(), process.get_filename() )
