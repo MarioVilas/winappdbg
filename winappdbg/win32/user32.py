a3a24545aa886747ac9863e4eac596842e662c9b
Mario Vilas <mvilas@gmail.com>
2010-07-21 16:17:52 +0000

    I must have been drunk when I wrote this...

--- winappdbg/win32/user32.py
+++ winappdbg/win32/user32.py
@@ -1352,13 +1352,13 @@ def GetPropA(hWnd, lpString):
     _GetPropA = windll.user32.GetPropA
     _GetPropA.argtypes = [HWND, LPSTR]
     _GetPropA.restype  = HANDLE
-    return _GetPropA(lpString)
+    return _GetPropA(hWnd, lpString)
 
 def GetPropW(hWnd, lpString):
     _GetPropW = windll.user32.GetPropW
     _GetPropW.argtypes = [HWND, LPWSTR]
     _GetPropW.restype  = HANDLE
-    return _GetPropW(lpString)
+    return _GetPropW(hWnd, lpString)
 
 GetProp = GuessStringType(GetPropA, GetPropW)
 
@@ -1372,14 +1372,14 @@ def SetPropA(hWnd, lpString, hData):
     _SetPropA.argtypes = [HWND, LPSTR, HANDLE]
     _SetPropA.restype  = BOOL
     _SetPropA.errcheck = RaiseIfZero
-    return _SetPropA(lpString)
+    _SetPropA(hWnd, lpString, hData)
 
 def SetPropW(hWnd, lpString, hData):
     _SetPropW = windll.user32.SetPropW
     _SetPropW.argtypes = [HWND, LPWSTR, HANDLE]
     _SetPropW.restype  = BOOL
     _SetPropW.errcheck = RaiseIfZero
-    return _SetPropW(lpString)
+    _SetPropW(hWnd, lpString, hData)
 
 SetProp = GuessStringType(SetPropA, SetPropW)
 
@@ -1391,13 +1391,13 @@ def RemovePropA(hWnd, lpString):
     _RemovePropA = windll.user32.RemovePropA
     _RemovePropA.argtypes = [HWND, LPSTR]
     _RemovePropA.restype  = HANDLE
-    return _RemovePropA(lpString)
+    return _RemovePropA(hWnd, lpString)
 
 def RemovePropW(hWnd, lpString):
     _RemovePropW = windll.user32.RemovePropW
     _RemovePropW.argtypes = [HWND, LPWSTR]
     _RemovePropW.restype  = HANDLE
-    return _RemovePropW(lpString)
+    return _RemovePropW(hWnd, lpString)
 
 RemoveProp = GuessStringType(RemovePropA, RemovePropW)
 
