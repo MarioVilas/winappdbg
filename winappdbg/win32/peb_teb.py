23ecd6e5637b287fcd39b3084c9d7be575bd9c20
Mario Vilas <mvilas@gmail.com>
2012-01-04 16:10:08 +0000

    Added PEB definition for Wow64 processes.

--- winappdbg/win32/peb_teb.py
+++ winappdbg/win32/peb_teb.py
@@ -1877,6 +1877,30 @@ class PEB(Structure):
         _fields_ = _PEB_W7_64._fields_
 PPEB = POINTER(PEB)
 
+# PEB structure for WOW64 processes.
+class PEB_32(Structure):
+    _pack_ = 8
+    if os == 'Windows NT':
+        _pack_   = _PEB_NT._pack_
+        _fields_ = _PEB_NT._fields_
+    elif os == 'Windows 2000':
+        _pack_   = _PEB_2000._pack_
+        _fields_ = _PEB_2000._fields_
+    elif os.startswith('Windows XP'):
+        _fields_ = _PEB_XP._fields_
+    elif os.startswith('Windows 2003 R2'):
+        _fields_ = _PEB_2003_R2._fields_
+    elif os.startswith('Windows 2003'):
+        _fields_ = _PEB_2003._fields_
+    elif os.startswith('Windows 2008 R2'):
+        _fields_ = _PEB_2008_R2._fields_
+    elif os.startswith('Windows 2008'):
+        _fields_ = _PEB_2008._fields_
+    elif os.startswith('Windows Vista'):
+        _fields_ = _PEB_Vista._fields_
+    else: #if os.startswith('Windows 7'):
+        _fields_ = _PEB_W7._fields_
+
 # from https://vmexplorer.svn.codeplex.com/svn/VMExplorer/src/Win32/Threads.cs
 #
 # [StructLayout (LayoutKind.Sequential, Size = 0x0C)]
