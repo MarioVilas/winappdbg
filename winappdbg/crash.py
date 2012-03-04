7452a727342479006004981ace5dc44993425fb7
Mario Vilas <mvilas@gmail.com>
2012-03-04 10:35:46 +0000

    * Several fixes to CrashContainer.  * Docstring fixes.

--- winappdbg/crash.py
+++ winappdbg/crash.py
@@ -1100,17 +1100,30 @@ class Crash (object):
 class CrashContainer (psyobj):
     """
     Manages a database of persistent Crash objects, trying to avoid duplicates.
-
     Uses a DBM database file for persistency.
 
     @warn:
-        DBM database support was deprecated in WinAppDbg 1.5. New applications
-        should not use this class.
+        DBM database support is provided for backwards compatibility with older
+        versions of WinAppDbg. New applications should not use this class.
+        Also, DBM databases in Python suffer from multiple problems that can
+        easily be avoided by switching to a SQL database.
+
+    @see: If you really must use a DBM database, try the standard C{shelve}
+        module instead: U{http://docs.python.org/library/shelve.html}
+
+    @group Marshalling configuration:
+        optimizeKeys, optimizeValues, compressKeys, compressValues, escapeKeys,
+        escapeValues, binaryKeys, binaryValues
 
     @type optimizeKeys: bool
-    @cvar optimizeKeys: C{True} to optimize the marshalling of keys, C{False}
-        otherwise. Only used with the C{pickle} module, ignored when using the
-        more secure C{cerealizer} module.
+    @cvar optimizeKeys: Ignored by the current implementation.
+
+        Up to WinAppDbg 1.4 this setting caused the database keys to be
+        optimized when pickled with the standard C{pickle} module.
+
+        But with a DBM database backend that causes inconsistencies, since the
+        same key can be serialized into multiple optimized pickles, thus losing
+        uniqueness.
 
     @type optimizeValues: bool
     @cvar optimizeValues: C{True} to optimize the marshalling of keys, C{False}
@@ -1151,59 +1164,32 @@ class CrashContainer (psyobj):
     binaryKeys      = False
     binaryValues    = False
 
-    def __init__(self, initialKeys = None):
+    def __init__(self, filename = None, allowRepeatedKeys = False):
         """
-        @type  initialKeys: dict( key S{->} marshalled key )
-        @param initialKeys:
-            (Optional) Use this dictionary (by reference) to add and match keys
-            to their marshalled counterparts.
+        @type  filename: str
+        @param filename: (Optional) File name for crash database.
+            If no filename is specified, the container is volatile.
+
+            Volatile containers are stored only in memory and
+            destroyed when they go out of scope.
+
+        @type  allowRepeatedKeys: bool
+        @param allowRepeatedKeys:
+            Currently not supported, always use C{False}.
         """
-        if initialKeys is None:
-            self.__keys = dict()
+        if allowRepeatedKeys:
+            raise NotImplementedError()
+        self.__filename = filename
+        if filename:
+            global anydbm
+            if not anydbm:
+                import anydbm
+            self.__db = anydbm.open(filename, 'c')
+            self.__keys = dict([ (self.unmarshall_key(mk), mk)
+                                                  for mk in self.__db.keys() ])
         else:
-            self.__keys = initialKeys
-
-    def __len__(self):
-        """
-        @rtype:  int
-        @return: Count of known keys.
-        """
-        return len(self.__keys)
-
-    def __bool__(self):
-        """
-        @rtype:  bool
-        @return: C{False} if there are no known keys.
-        """
-        return bool(self.__keys)
-
-    def __contains__(self, crash):
-        """
-        @type  crash: L{Crash}
-        @param crash: Crash object.
-
-        @rtype:  bool
-        @return:
-            C{True} if a Crash object with the same key is in the container.
-        """
-        return self.has_key( crash.key() )
-
-    def has_key(self, key):
-        """
-        @type  key: L{Crash} key.
-        @param key: Key to find.
-
-        @rtype:  bool
-        @return: C{True} if the key is present in the set of known keys.
-        """
-        return key in self.__keys
-
-    def iterkeys(self):
-        """
-        @rtype:  iterator
-        @return: Iterator of known L{Crash} keys.
-        """
-        return self.__keys.iterkeys()
+            self.__db = dict()
+            self.__keys = dict()
 
     def remove_key(self, key):
         """
@@ -1228,13 +1214,7 @@ class CrashContainer (psyobj):
         """
         if key in self.__keys:
             return self.__keys[key]
-        if self.optimizeKeys:
-            # May return different marshalled versions for the same key.
-            skey = pickle.dumps(key, protocol = HIGHEST_PROTOCOL)
-            skey = optimize(skey)
-        else:
-            # Always returns the same marshalled version for each key.
-            skey = pickle.dumps(key, protocol = 0)
+        skey = pickle.dumps(key, protocol = 0)
         if self.compressKeys:
             skey = zlib.compress(skey, zlib.Z_BEST_COMPRESSION)
         if self.escapeKeys:
@@ -1265,7 +1245,7 @@ class CrashContainer (psyobj):
     def marshall_value(self, value, storeMemoryMap = False):
         """
         Marshalls a Crash object to be used in the database.
-        The C{memoryMap} member is B{NOT} stored here.
+        By default the C{memoryMap} member is B{NOT} stored here.
 
         @warning: Setting the C{storeMemoryMap} argument to C{True} can lead to
             a severe performance penalty!
@@ -1328,6 +1308,48 @@ class CrashContainer (psyobj):
     # Other methods like get, has_key, iterkeys and itervalues
     # are dictionary-like.
 
+    def __len__(self):
+        """
+        @rtype:  int
+        @return: Count of known keys.
+        """
+        return len(self.__keys)
+
+    def __bool__(self):
+        """
+        @rtype:  bool
+        @return: C{False} if there are no known keys.
+        """
+        return bool(self.__keys)
+
+    def __contains__(self, crash):
+        """
+        @type  crash: L{Crash}
+        @param crash: Crash object.
+
+        @rtype:  bool
+        @return:
+            C{True} if a Crash object with the same key is in the container.
+        """
+        return self.has_key( crash.key() )
+
+    def has_key(self, key):
+        """
+        @type  key: L{Crash} key.
+        @param key: Key to find.
+
+        @rtype:  bool
+        @return: C{True} if the key is present in the set of known keys.
+        """
+        return key in self.__keys
+
+    def iterkeys(self):
+        """
+        @rtype:  iterator
+        @return: Iterator of known L{Crash} keys.
+        """
+        return self.__keys.iterkeys()
+
     class __CrashContainerIterator (object):
         """
         Iterator of Crash objects. Returned by L{CrashContainer.__iter__}.
@@ -1356,37 +1378,6 @@ class CrashContainer (psyobj):
             """
             key  = self.__keys_iter.next()
             return self.__container.get(key)
-
-    def __init__(self, filename = None, allowRepeatedKeys = False):
-        """
-        @type  filename: str
-        @param filename: (Optional) File name for crash database.
-            If no filename is specified, the container is volatile.
-
-            Volatile containers are stored only in memory and
-            destroyed when they go out of scope.
-
-        @type  allowRepeatedKeys: bool
-        @param allowRepeatedKeys:
-            Currently not supported, always use C{False}.
-        """
-        warnings.warn(
-            "The %s class is deprecated since WinAppDbg 1.5." % self.__class__,
-            DeprecationWarning)
-        if allowRepeatedKeys:
-            raise NotImplementedError()
-        self.__filename = filename
-        if filename:
-            global anydbm
-            if not anydbm:
-                import anydbm
-            self.__db = anydbm.open(filename, 'c')
-            keys = dict([ (self.unmarshall_key(mk), mk)
-                                                  for mk in self.__db.keys() ])
-            ContainerBase.__init__(self, keys)
-        else:
-            self.__db = dict()
-            ContainerBase.__init__(self)
 
     def __del__(self):
         "Class destructor. Closes the database when this object is destroyed."
@@ -1435,6 +1426,17 @@ class CrashContainer (psyobj):
             data = self.marshall_value(crash, storeMemoryMap = True)
             self.__db[skey] = data
 
+    def __delitem__(self, key):
+        """
+        Removes a crash from the container.
+
+        @type  crash: L{Crash}
+        @param crash: Crash object to remove.
+        """
+        skey = self.marshall_key(key)
+        del self.__db[skey]
+        self.remove_key(key)
+
     def remove(self, crash):
         """
         Removes a crash from the container.
@@ -1442,10 +1444,7 @@ class CrashContainer (psyobj):
         @type  crash: L{Crash}
         @param crash: Crash object to remove.
         """
-        key  = crash.key()
-        skey = self.marshall_key(key)
-        del self.__db[skey]
-        self.remove_key(key)
+        del self[ crash.key() ]
 
     def get(self, key):
         """
@@ -1470,6 +1469,27 @@ class CrashContainer (psyobj):
         data  = self.__db[skey]
         crash = self.unmarshall_value(data)
         return crash
+
+    def __getitem__(self, key):
+        """
+        Retrieves a crash from the container.
+
+        @type  key: L{Crash} unique key.
+        @param key: Key of the crash to get.
+
+        @rtype:  L{Crash} object.
+        @return: Crash matching the given key.
+
+        @see:     L{iterkeys}
+        @warning: A B{copy} of each object is returned,
+            so any changes made to them will be lost.
+
+            To preserve changes do the following:
+                1. Keep a reference to the object.
+                2. Delete the object from the set.
+                3. Modify the object and add it again.
+        """
+        return self.get(key)
 
 #==============================================================================
 
@@ -1607,7 +1627,9 @@ class CrashTable(CrashDictionary):
     """
     Old crash dump persistencer using a SQLite database.
 
-    Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+    @warn:
+        Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+        New applications should not use this class.
     """
 
     def __init__(self, location = None, allowRepeatedKeys = True):
@@ -1640,7 +1662,9 @@ class CrashTableMSSQL (CrashDictionary):
     """
     Old crash dump persistencer using a Microsoft SQL Server database.
 
-    Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+    @warn:
+        Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+        New applications should not use this class.
     """
 
     def __init__(self, location = None, allowRepeatedKeys = True):
@@ -1667,7 +1691,9 @@ class VolatileCrashContainer (CrashTable):
     """
     Old in-memory crash dump storage.
 
-    Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+    @warn:
+        Superceded by L{CrashDictionary} since WinAppDbg 1.5.
+        New applications should not use this class.
     """
 
     def __init__(self, allowRepeatedKeys = True):
