c4df912924917881ddd984ebb36b808a0e872920
Mario Vilas <mvilas@gmail.com>
2012-05-15 11:22:54 +0000

    Added a warning when hardware breakpoints are overwritten by watch_variable. TO DO: change the API of watch_variable to match watch_buffer.

--- winappdbg/breakpoint.py
+++ winappdbg/breakpoint.py
@@ -4160,7 +4160,9 @@ class _BreakpointContainer (object):
         """
 
         # TODO
-        # Maybe we could merge the breakpoints instead of overwriting them.
+        # We should merge the breakpoints instead of overwriting them.
+        # We'll have the same problem as watch_buffer and we'll need to change
+        # the API again.
 
         if size == 1:
             sizeFlag = self.BP_WATCH_BYTE
@@ -4172,7 +4174,14 @@ class _BreakpointContainer (object):
             sizeFlag = self.BP_WATCH_QWORD
         else:
             raise ValueError("Bad size for variable watch: %r" % size)
+
         if self.has_hardware_breakpoint(tid, address):
+            warnings.warn(
+                "Hardware breakpoint in thread %d at address %s was overwritten!" \
+                % (tid, HexDump.address(address,
+                                   self.system.get_thread(tid).get_bits())),
+                BreakpointWarning)
+
             bp = self.get_hardware_breakpoint(tid, address)
             if  bp.get_trigger() != self.BP_BREAK_ON_ACCESS or \
                 bp.get_watch()   != sizeFlag:
@@ -4180,10 +4189,12 @@ class _BreakpointContainer (object):
                     self.define_hardware_breakpoint(tid, address,
                                self.BP_BREAK_ON_ACCESS, sizeFlag, True, action)
                     bp = self.get_hardware_breakpoint(tid, address)
+
         else:
             self.define_hardware_breakpoint(tid, address,
                                self.BP_BREAK_ON_ACCESS, sizeFlag, True, action)
             bp = self.get_hardware_breakpoint(tid, address)
+
         return bp
 
     def __clear_variable_watch(self, tid, address):
