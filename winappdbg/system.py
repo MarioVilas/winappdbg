af3e19158c4774e09910162650fbc9740f18473b
Mario Vilas <mvilas@gmail.com>
2012-12-16 00:09:02 +0000

    * added System.get_service_display_name()  * fixed docs of System.get_service()

--- winappdbg/system.py
+++ winappdbg/system.py
@@ -75,7 +75,8 @@ class System (_ProcessContainer):
     @group System services:
         get_services, get_active_services,
         start_service, stop_service,
-        pause_service, resume_service
+        pause_service, resume_service,
+        get_service_display_name
 
     @group Miscellaneous global settings:
         set_kill_on_exit_mode, read_msr, write_msr, enable_step_on_branch_mode,
@@ -794,7 +795,7 @@ class System (_ProcessContainer):
             C{ServiceName} member of the service descriptors returned by
             L{get_services} or L{get_active_services}.
 
-        @rtype:  L{win32.ServiceStatusProcessEntry}
+        @rtype:  L{win32.ServiceStatusProcess}
         @return: Service status descriptor.
         """
         with win32.OpenSCManager(
@@ -807,6 +808,26 @@ class System (_ProcessContainer):
                     return win32.QueryServiceStatusEx(hService)
                 except AttributeError:
                     return win32.QueryServiceStatus(hService)
+
+    @staticmethod
+    def get_service_display_name(name):
+        """
+        Get the service display name for the given service name.
+
+        @see: L{get_service}
+
+        @type  name: str
+        @param name: Service unique name. You can get this value from the
+            C{ServiceName} member of the service descriptors returned by
+            L{get_services} or L{get_active_services}.
+
+        @rtype:  str
+        @return: Service display name.
+        """
+        with win32.OpenSCManager(
+            dwDesiredAccess = win32.SC_MANAGER_ENUMERATE_SERVICE
+        ) as hSCManager:
+            return win32.GetServiceDisplayName(hSCManager, name)
 
     @staticmethod
     def start_service(name, argv = None):
