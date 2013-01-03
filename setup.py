e4b6b12bff5b494768ca16f3d92256f03840790a
Mario Vilas <mvilas@gmail.com>
2013-01-03 16:46:51 +0000

    Added Topic :: Security

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ metadata = {
                         'Programming Language :: Python :: 2.5',
                         'Programming Language :: Python :: 2.6',
                         'Programming Language :: Python :: 2.7',
+                        'Topic :: Security',
                         'Topic :: Software Development :: Debuggers',
                         'Topic :: Software Development :: Libraries :: Python Modules',
                         ],
