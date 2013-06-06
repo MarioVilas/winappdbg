d3db4a4fcae2cd52f6911593c7397ed28c0811c2
Mario Vilas <mvilas@gmail.com>
2013-06-06 13:32:01 +0000

    More verbose output.

--- install.bat
+++ install.bat
@@ -10,8 +10,14 @@ if "%1"=="/h" goto Help
 if "%1"=="/H" goto Help
 if "%1"=="-h" goto Help
 if "%1"=="--help" goto Help
-if not exist "%1" goto Help
+if not exist "%1" goto NotExistHelp
 goto Specific
+
+:NotExistHelp
+echo Error: Python interpreter not found: %1
+echo.
+echo Run %0 /? for more help.
+goto Exit
 
 :Help
 echo Installer script for WinAppDbg
@@ -30,11 +36,15 @@ for /f "delims=#" %%P in (install.cfg) do cmd /c if exist "%%P" echo     %%P
 goto Exit
 
 :Default
+echo Installing on the default Python installation...
+echo.
 python setup.py install
 python test.py
 goto Exit
 
 :Specific
+echo Installing on: %1
+echo.
 "%1" setup.py install
 "%1" test.py
 goto Exit
