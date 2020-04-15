b0c29390e2d5d1578411fd16fb0068528cb102d3
Mario Vilas <mvilas@gmail.com>
2020-04-15 10:43:45 +0200

    Fixed #60

--- winappdbg/module.py
+++ winappdbg/module.py
@@ -585,23 +585,6 @@ class Module (object):
                 if symbol == SymbolName.lower():
                     return SymbolAddress
 
-    def get_symbol_from_list(self,address):
-        found = None
-        SymList = {}
-        SortedSymList = {}
-
-        for (SymbolName, SymbolAddress, SymbolSize) in self.iter_symbols():
-            SymList[SymbolAddress] = SymbolName
-        SortedSymList = sorted(SymList.items())
-        for SymbolAddress,SymbolName in SortedSymList:
-            if SymbolAddress <= address:
-                SymbolStartAddress = SymbolAddress
-                SymbolStartName = SymbolName
-            else:
-                break
-        found = (SymbolStartName, SymbolStartAddress, 0)
-        return found
-
     def get_symbol_at_address(self, address):
         """
         Tries to find the closest matching symbol for the given address.
@@ -616,17 +599,17 @@ class Module (object):
              - Size (in bytes)
             Returns C{None} if no symbol could be matched.
         """
-        found = None
-        for (SymbolName, SymbolAddress, SymbolSize) in self.iter_symbols():
-
+        result = None
+        symbols = [
+            (SymbolAddress, SymbolName)
+            for (SymbolName, SymbolAddress, SymbolSize) in self.iter_symbols()
+            ]
+        symbols.sort()
+        for SymbolAddress, SymbolName in symbols:
             if SymbolAddress > address:
-                continue
-            if SymbolAddress + SymbolSize > address:
-                if not found or found[1] < SymbolAddress:
-                    found = (SymbolName, SymbolAddress, SymbolSize)
-        if found == None:
-            found = self.get_symbol_from_list(address)
-        return found
+                break
+            result = (SymbolName, SymbolAddress, 0)
+        return result
 
 #------------------------------------------------------------------------------
 
